30c937436c30be906d501901cf9ed88f4b175dd1
blitzfurz <114784815+blitzfurz@users.noreply.github.com>
2022-11-09 14:27:37 +0100

    Update main.py
    Change main because

--- main.py
+++ main.py
@@ -1,10 +1,7 @@
 # TODO: please help us write the documentation, how in the word is anyone going to
 # understand this you guys!!!!
 def is_one_greater_than_two():
-    if 1 > 2:
-        print(True)
-    else:
-        print(False)
+    print('*')
 
 
 if __name__ == '__main__':
